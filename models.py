829f4a7080426553b5be89d0ba90c3f9557e6b0b
watremons <z2331355763@sina.com>
2022-10-30 17:05:13 +0800

    🐞 fix(models): 修改类的print str

--- models.py
+++ models.py
@@ -8,36 +8,37 @@ class Literal:
     Method:
         None
     '''
-    def __init__(self, variable: int, sign: int) -> None:
+    def __init__(self, variable: int, sign: int, literal: int) -> None:
         """
         Method:
             Constructed Funtion
         """
         self.variable = variable
         self.sign = sign
+        self.literal = literal
 
     def __str__(self) -> str:
         """
         Method:
             Formatted print string
         """
-        return self.variable
+        return str(self.variable) if not self.sign else '-{0}'.format(self.variable)
 
 class Clause:
     '''
     Class:
         Definition of literal
     Attributes:
-        literalList: a list of literals denoted to a disjunctive normal form of them
+        literal_list: a list of literals denoted to a disjunctive normal form of them
     Method:
         None
     '''
-    def __init__(self, literalList: list[Literal]) -> None:
+    def __init__(self, literal_list: list[Literal]) -> None:
         """
         Method:
             Constructed Funtion
         """
-        self.literalList = literalList
+        self.literal_list = literal_list
 
     def __str__(self) -> str:
         """
@@ -45,10 +46,10 @@ class Clause:
             Formatted print string
         """
         description = ""
-        for index, literal in enumerate(self.literalList):
-            description += "x{0}".format(literal)
-            if index != len(self.literalList):
-                description += "∪"
+        for index, literal in enumerate(self.literal_list):
+            description += "x{0}".format(literal.variable) if not literal.sign else "-x{0}".format(literal.variable) 
+            if index != len(self.literal_list)-1:
+                description += "V"
         return description
 
 class Cnf:
@@ -56,17 +57,17 @@ class Cnf:
     Class:
         Definition of Cnf
     Attributes:
-        clauseList: a list of clauses denoted to a conjunctive normal form of them
+        clause_list: a list of clauses denoted to a conjunctive normal form of them
     Method:
         None
     '''
-    def __init__(self, clauseList: list[Clause], literalNum: int) -> None:
+    def __init__(self, clause_list: list[Clause], literal_num: int) -> None:
         """
         Method:
             Constructed Funtion
         """
-        self.clauseList = clauseList
-        self.literalNum = literalNum
+        self.clause_list = clause_list
+        self.literal_num = literal_num
 
     def __str__(self) -> str:
         """
@@ -74,10 +75,10 @@ class Cnf:
             Formatted print string
         """
         description = ""
-        for index, clause in enumerate(self.clauseList):
+        for index, clause in enumerate(self.clause_list):
             description += "({0})".format(clause.__str__())
-            if index != len(self.literalList):
-                description += "∩"
+            if index != len(self.clause_list)-1:
+                description += "^"
         return description
 class Node:
     '''
@@ -115,18 +116,18 @@ class DecisionLevel:
     Class:
         Definition of Decision Level, which is created when decide the value of a literal
     Attributes:
-        nodeList: the list of node which are included in this decision level
+        node_list: the list of node which are included in this decision level
         level: the level number of a decision level
     Method:
         None
     """
 
-    def __init__(self, nodeList: list[Node], level: int) -> None:
+    def __init__(self, node_list: list[Node], level: int) -> None:
         """
         Method:
             Constructed Function
         """
-        self.nodeList = nodeList
+        self.node_list = node_list
         self.level = level
 
     def __str__(self) -> str:
@@ -136,7 +137,7 @@ class DecisionLevel:
         """
         description = ""
         description += "Decision Level {0}\n".format(self.level)
-        for index, node in enumerate(self.nodeList):
+        for index, node in enumerate(self.node_list):
             description += "\tNode {0}:\n".format(index)
             description += "\tliteral: {0}\n".format(node.literal)
             description += "\treason: {0}\n".format(node.reason)
@@ -148,17 +149,17 @@ class Trail:
     Class:
         Definition of Trail, which is stored several decision levels linearly
     Attributes:
-        decisionLevelList: a list of decision level which are included in a trail
+        decision_level_list: a list of decision level which are included in a trail
     Method:
         None
     """
 
-    def __init__(self, decisionLevelList: list) -> None:
+    def __init__(self, decision_level_list: list) -> None:
         """
         Method:
             Constructed Funcion
         """
-        self.decisionLevelList = decisionLevelList
+        self.decision_level_list = decision_level_list
 
     def __str__(self) -> str:
         """
@@ -167,8 +168,8 @@ class Trail:
         """
         description = ""
         description += "Trail:"
-        for decisionLevel in self.decisionLevelList:
-            for node in decisionLevel.nodeList:
+        for decision_level in self.decision_level_list:
+            for node in decision_level.node_list:
                 description += "{0}\t{1}\t{2}\n".format(node.literal, node.reason, node.level)
 
 if __name__ == "__main__":
