1f5ea036a7a125abce36cee483a9055b09bdccc8
watremons <z2331355763@sina.com>
2022-11-02 16:59:50 +0800

    ✨ feat(solver): 新增solver基础部分

--- solver.py
+++ solver.py
@@ -11,7 +11,6 @@ class SatSolver:
         cnf: a Cnf class which store the origin Cnf
         trail: a literal sequence with result
     Methods:
-        parser: parse the cnf file to a Cnf Class
         solve: solve the SAT Problem by CDCL algorithm
     """
     def __init__(self, cnf: Cnf):
@@ -23,12 +22,54 @@ class SatSolver:
         self.trail = Trail(decision_level_list=[])
         # Use assignments to record the real value of every variable
         self.assignments = dict()
+        self.now_decision_level = 0;
         for clause in cnf.clause_list:
             for literal in clause.literal_list:
                 if literal.variable not in self.assignments and (literal.variable-cnf.literal_num) not in self.assignments:
                     # literal or (not literal) are both not in assignments
                     self.assignments[literal.variable] = None
 
+    def unit_propagate(self, cnf_copy):
+        pass
+
+    def conflict_analyze(self, cnf_copy) -> Clause:
+        pass
+
+    def backtrack(self):
+        pass
+
+    def make_new_decision_level(self):
+        pass
+
+    def get_new_unknown_literal(self, cnf_copy):
+        pass
+
+    def unknown_variable_exists(self):
+        pass
+
+    def has_flase_clause(self):
+        pass
+
+    def solve(self):
+        cnf_copy = self.cnf
+        while True:
+            # do BCP process
+            cnf_copy = self.unit_propagate(cnf_copy)
+            # do "conflict analysis" process
+            if self.has_flase_clause(cnf_copy):
+                if self.now_decision_level == 0:
+                    return False
+                new_clause = self.conflict_analyze(cnf_copy)
+                self.cnf.append_clause(new_clause)
+                # do BACKTRACK process
+                self.backtrack()
+            else:
+                if not self.unknown_variable_exists(cnf_copy):
+                    return True
+                # do DECIDE process
+                self.make_new_decision_level()
+                l = self.get_new_unknown_literal(cnf_copy)
+                cnf_copy = cnf_copy.append(l)
 
 if __name__ == "__main__":
     cnf = cnf_parse("./raw/test.cnf")
