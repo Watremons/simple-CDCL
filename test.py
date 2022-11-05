813c993e35f6a66b7313ad39bf4d017f55ee92fb
watremons <z2331355763@sina.com>
2022-11-05 11:17:14 +0800

    🧪 test(test): 新增conflict测试用例

--- test.py
+++ test.py
@@ -1,29 +1,22 @@
-from symtable import Symbol
-from typing import Union
-from sympy import Symbol
-from sympy.logic.boolalg import Not
+from models import Node, DecisionLevel, Trail
 
-print(Symbol)
-print(Not)
-print(Union[Symbol, Not])
-
-def cdcl(cnf):
-    cnf_copy = cnf
-    while true:
-        # do BCP process
-        cnf_copy = unitPropagate(cnf, cnf_copy)
-        # do "conflict analysis" process
-        if hasAFlaseClause(cnf_copy):
-            if nowDecisionLevel == 0:
-                return False
-            new_clause = conflictAnalyze(cnf_copy, cnf)
-            cnf = cnf.append(new_clause)
-            # do BACKTRACK process 
-            back2TargetDecisionLevel()
-        else:
-            if not valueOfVariablesIsUnknown(cnf_copy):
-                return True
-            # do DECIDE process
-            makeANewDecisionLevel()
-            l = getNewLiteralWhoseValueIsUnknown(cnf_copy);
-            cnf_copy = cnf_copy.append(l)
+if __name__ == "__main__":
+    trail = Trail(decision_level_list=[
+        DecisionLevel(level=1, node_list=[
+            Node(variable=1, value=True, reason=None, level=1),
+            Node(variable=2, value=False, reason=[1], level=1),
+            Node(variable=3, value=True, reason=[1], level=1),
+            Node(variable=4, value=False, reason=[3], level=1),
+            Node(variable=5, value=True, reason=[2, 4], level=1),
+        ]),
+        DecisionLevel(level=2, node_list=[
+            Node(variable=6, value=False, reason=None, level=2),
+            Node(variable=7, value=False, reason=[5, 6], level=2),
+            Node(variable=8, value=True, reason=[2, 7], level=2),
+            Node(variable=9, value=False, reason=[8], level=2),
+            Node(variable=10, value=True, reason=[8], level=2),
+            Node(variable=11, value=True, reason=[9, 10], level=2),
+            Node(variable=12, value=False, reason=[10], level=2),
+            Node(variable=None, value=True, reason=[11, 12], level=2),
+        ])
+    ])
