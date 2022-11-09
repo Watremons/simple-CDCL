8d991c0d3aca6d12ef99a6dc9ef565115e45f862
watremons <z2331355763@sina.com>
2022-11-09 19:43:01 +0800

    🦄 refactor(utils): 删除不必要的util函数

--- utils.py
+++ utils.py
@@ -2,65 +2,6 @@
 from models import Clause, Literal, Node
 
 
-def to_variable(literal: int, variable_num: int) -> tuple[int, int]:
-    """
-    Method:
-        Trans literal to variable
-    Params:
-        literal: a num denoted the literal
-        variable_num: the variable num in the cnf
-    Return:
-        variable
-        the sign of the variable
-    """
-    if literal <= variable_num:
-        return literal, 1
-    else:
-        return literal - variable_num, 0
-
-
-def to_literal(variable: int, sign: int, variable_num: int) -> int:
-    """
-    Method:
-        Trans variable to literal
-    Params:
-        variable: variable num
-        sign: variable sign
-        variable_num: the variable num in the cnf
-    Return:
-        literal
-    """
-    if sign:
-        return variable
-    else:
-        return variable + variable_num
-
-
-def to_clause(node: Node, variable_num: int) -> Clause:
-    """
-    Method:
-        Transform a node to a clause whose literal_list consists of the variable combined value and the reason of node
-    Params:
-        node: the node to be transformed
-        variable_num: the variable num in the cnf
-    Return:
-        a clause transform from input node
-    """
-    reason_literal_list = []
-    if node.variable is not None:
-        reason_literal_list.append(
-            Literal(
-                variable=node.variable,
-                sign=node.value,
-                literal=to_literal(variable=node.variable, sign=node.value, variable_num=variable_num)
-            )
-        )
-    for reason_literal in node.reason:
-        variable, sign = to_variable(reason_literal, variable_num)
-        reason_literal_list.append(Literal(variable=variable, sign=sign, literal=reason_literal))
-    return Clause(literal_list=reason_literal_list)
-
-
 def resolute_clause(conflict_clause: Clause, to_resolute_clause: Clause, variable: int, variable_num: int) -> Clause:
     """
     Method:
