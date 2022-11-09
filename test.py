9f190c7011ee78843f1d5d7e584c40be3027a1ee
watremons <z2331355763@sina.com>
2022-11-09 16:52:32 +0800

    🧪 test(test): 修改测试用例以适配新的Node数据结构

--- test.py
+++ test.py
@@ -8,58 +8,58 @@ if __name__ == "__main__":
 
     trail = Trail(node_list=[
         Node(variable=1, value=True, reason=None, level=1, index=0),
-        Node(variable=2, value=False, reason=[13], level=1, index=1),
-        Node(variable=3, value=True, reason=[13], level=1, index=2),
-        Node(variable=4, value=False, reason=[15], level=1, index=3),
-        Node(variable=5, value=True, reason=[2, 4], level=1, index=4),
+        Node(variable=2, value=False, reason=0, level=1, index=1),
+        Node(variable=3, value=True, reason=1, level=1, index=2),
+        Node(variable=4, value=False, reason=2, level=1, index=3),
+        Node(variable=5, value=True, reason=3, level=1, index=4),
         Node(variable=6, value=False, reason=None, level=2, index=5),
-        Node(variable=7, value=False, reason=[17, 6], level=2, index=6),
-        Node(variable=8, value=True, reason=[2, 7], level=2, index=7),
-        Node(variable=9, value=False, reason=[20], level=2, index=8),
-        Node(variable=10, value=True, reason=[20], level=2, index=9),
-        Node(variable=11, value=True, reason=[9, 22], level=2, index=10),
-        Node(variable=12, value=False, reason=[22], level=2, index=11),
-        Node(variable=None, value=True, reason=[23, 12], level=2, index=12),
+        Node(variable=7, value=False, reason=4, level=2, index=6),
+        Node(variable=8, value=True, reason=5, level=2, index=7),
+        Node(variable=9, value=False, reason=6, level=2, index=8),
+        Node(variable=10, value=True, reason=7, level=2, index=9),
+        Node(variable=11, value=True, reason=8, level=2, index=10),
+        Node(variable=12, value=False, reason=9, level=2, index=11),
+        Node(variable=None, value=True, reason=10, level=2, index=12),
     ])
 
     node_list = []
     new_node = Node(variable=1, value=True, reason=None, level=1, index=0)
     node_list.append(new_node)
     variable_to_node[1] = new_node
-    new_node = Node(variable=2, value=False, reason=[13], level=1, index=1)
+    new_node = Node(variable=2, value=False, reason=0, level=1, index=1)
     node_list.append(new_node)
     variable_to_node[2] = new_node
-    new_node = Node(variable=3, value=True, reason=[13], level=1, index=2)
+    new_node = Node(variable=3, value=True, reason=1, level=1, index=2)
     node_list.append(new_node)
     variable_to_node[3] = new_node
-    new_node = Node(variable=4, value=False, reason=[15], level=1, index=3)
+    new_node = Node(variable=4, value=False, reason=2, level=1, index=3)
     node_list.append(new_node)
     variable_to_node[4] = new_node
-    new_node = Node(variable=5, value=True, reason=[2, 4], level=1, index=4)
+    new_node = Node(variable=5, value=True, reason=3, level=1, index=4)
     node_list.append(new_node)
     variable_to_node[5] = new_node
     new_node = Node(variable=6, value=False, reason=None, level=2, index=5)
     node_list.append(new_node)
     variable_to_node[6] = new_node
-    new_node = Node(variable=7, value=False, reason=[17, 6], level=2, index=6)
+    new_node = Node(variable=7, value=False, reason=4, level=2, index=6)
     node_list.append(new_node)
     variable_to_node[7] = new_node
-    new_node = Node(variable=8, value=True, reason=[2, 7], level=2, index=7)
+    new_node = Node(variable=8, value=True, reason=5, level=2, index=7)
     node_list.append(new_node)
     variable_to_node[8] = new_node
-    new_node = Node(variable=9, value=False, reason=[20], level=2, index=8)
+    new_node = Node(variable=9, value=False, reason=6, level=2, index=8)
     node_list.append(new_node)
     variable_to_node[9] = new_node
-    new_node = Node(variable=10, value=True, reason=[20], level=2, index=9)
+    new_node = Node(variable=10, value=True, reason=7, level=2, index=9)
     node_list.append(new_node)
     variable_to_node[10] = new_node
-    new_node = Node(variable=11, value=True, reason=[9, 22], level=2, index=10)
+    new_node = Node(variable=11, value=True, reason=8, level=2, index=10)
     node_list.append(new_node)
     variable_to_node[11] = new_node
-    new_node = Node(variable=12, value=False, reason=[22], level=2, index=11)
+    new_node = Node(variable=12, value=False, reason=9, level=2, index=11)
     node_list.append(new_node)
     variable_to_node[12] = new_node
-    new_node = Node(variable=None, value=True, reason=[23, 12], level=2, index=12)
+    new_node = Node(variable=None, value=True, reason=10, level=2, index=12)
     node_list.append(new_node)
 
     cnf = cnf_parse("./raw/test2.cnf")
